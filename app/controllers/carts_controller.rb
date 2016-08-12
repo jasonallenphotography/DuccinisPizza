79aee2a9a1e964b28801ac728d8c081d86b348ea
Jason Allen <jasonallenphotography@gmail.com>
2016-08-12 16:08:11 -0400

    Add emptycart method to Carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,4 +2,11 @@ class CartsController < ApplicationController
   def show
     @order_items = current_order.order_items
   end
+
+  def emptycart
+    current_order.order_items.each do |item|
+      item.delete
+    end
+    redirect_to '/carts/show'
+  end
 end
