d34210f511af71bca880820bbd65dc619d64c14c
Jason Allen <jasonallenphotography@gmail.com>
2016-08-12 16:09:28 -0400

    Add product child classes to Product#index method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    @salads = Salad.all
+    @sides = Side.all
+    @wings = Wing.all
+    @subs = Sub.all
+    @pizzas = Pie.all
+    
     @order_item = current_order.order_items.new
   end
 end
