9d52cc172cd7642b16f41cc051fecbe2dd370e03
Jason Allen <jasonallenphotography@gmail.com>
2016-08-12 15:23:03 -0400

    Add OrderItem methods,associations,validations to model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812191929) do
+ActiveRecord::Schema.define(version: 20160812192153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.decimal  "unit_price",  precision: 12, scale: 3
+    t.integer  "quantity"
+    t.decimal  "total_price", precision: 12, scale: 3
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+  end
+
+  add_index "order_items", ["order_id"], name: "index_order_items_on_order_id", using: :btree
+  add_index "order_items", ["product_id"], name: "index_order_items_on_product_id", using: :btree
 
   create_table "order_statuses", force: :cascade do |t|
     t.string   "name"
@@ -55,5 +68,7 @@ ActiveRecord::Schema.define(version: 20160812191929) do
     t.datetime "updated_at",       null: false
   end
 
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
   add_foreign_key "orders", "order_statuses"
 end
