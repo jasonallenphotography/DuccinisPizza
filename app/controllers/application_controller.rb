4bd7a5907c4a40d8be9e72d7e9d1604226316e45
Jason Allen <jasonallenphotography@gmail.com>
2016-08-12 16:07:02 -0400

    Additional application controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :current_order
+  helper_method :current_user, :current_order, :logged_in?
 
   def current_order
     if !session[:order_id].nil?
@@ -13,4 +13,21 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def allow_user
+    redirect_to root_path unless logged_in?
+  end
+
+  def logged_in?
+    session[:user_id] && current_user.present?
+  end
+
+  def redirect_unless_logged_in
+    redirect_to '/' unless logged_in?
+  end
+
+  def redirect_if_already_logged_in
+    redirect_to '/' if logged_in?
+  end
+
 end
