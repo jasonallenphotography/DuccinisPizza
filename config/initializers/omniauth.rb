dc3db94b2bbba8c25d8c5132469a4cd7670c8399
Jason Allen <jasonallenphotography@gmail.com>
2016-08-12 13:56:50 -0400

    Build initializer, add APP ID and Secret to secrets.yml, add security certs to root

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,6 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
-  '783634415990-g7frsbkahcf67btfgo3k2dt3t5343824.apps.googleusercontent.com,
-  'uO9DhBVk0liy7Ajc7SHSbLLY',
-  {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  Rails.application.secrets.GOOGLE_APP_ID,
+  Rails.application.secrets.GOOGLE_APP_SECRET, {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
