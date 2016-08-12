fa448b7d553d7edcec58dd71542a9332cac85734
Jason Allen <jasonallenphotography@gmail.com>
2016-08-12 15:27:30 -0400

    Add type to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812192153) do
+ActiveRecord::Schema.define(version: 20160812192428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20160812192153) do
     t.string   "email"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "type"
   end
 
   add_foreign_key "order_items", "orders"
